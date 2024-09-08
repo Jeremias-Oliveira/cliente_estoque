c602d39a9767221960b7b03153df33cec931722b
Jeremias-Oliveira <jere2124512@gmail.com>
2024-09-08 15:25:18 -0300

    Atualização v3.1

--- paginas/home.php
+++ paginas/home.php
@@ -16,8 +16,30 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../assets/css/home-layout.css">
 </head>
 <body>
-    <h1>Você acessou o Sistema!</h1>
+    <nav class="menu-lateral">
+        <div class="btn-exp">
+            <i class="bi bi-list" id="btn-expandir"></i>
+        </div>
+
+        <ul>
+            <li class="item-menu ativo">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-people"></i></span>
+                    <span class="txt-link">Cliente</span>
+                </a>
+            </li>
+            <li class="item-menu">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-box-seam"></i></span>
+                    <span class="txt-link">Estoque</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <script src="../assets/js/menu-lateral.js"></script>
 </body>
 </html>
